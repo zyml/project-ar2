7529c5669850799a72c1781b5f421db41d63333f
zyml <zy@zy.sg>
2012-08-11 03:54:23 +0800

    per-line display type will now show non-relative post dates

--- section-line.php
+++ section-line.php
@@ -6,7 +6,7 @@
 ?>
 
 <li <?php post_class() ?>>
-	<span class="entry-date"><?php ar2_posted_on() ?></span>
+	<span class="entry-date"><?php the_time( get_option( 'date_format' ) ) ?></span>
 	<h3 class="entry-title"><a rel="bookmark" href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'ar2'), get_the_title() ) ?>"><?php the_title() ?></a></h3>
 	<a class="entry-comments" href="<?php comments_link() ?>"><?php comments_number() ?></a>
 </li>
